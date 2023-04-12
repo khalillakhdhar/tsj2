f033acc1cbefdbf0ddb822e20e4d450b8db5d569
khalil <khalillakhdharatc@gmail.com>
2023-04-12 08:48:04 +0100

    update

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tableau_1 = require("./tableau");
+const tableau = ['a', 'b', 'c'];
+const element = 'b';
+const result = (0, tableau_1.findElement)(tableau, element);
+console.log(result);
+//# sourceMappingURL=index.js.map
---
+++ dist/tableau.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.findElement = void 0;
+function findElement(tableau, element) {
+    return tableau.includes(element);
+}
+exports.findElement = findElement;
+//# sourceMappingURL=tableau.js.map
